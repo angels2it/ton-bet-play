5acbd6e448b7e7764edba4183f4ab5fff93c24c6
angels2it <angels2it@gmail.com>
2025-01-19 09:25:30 +0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { TonConnectUIProvider, TonConnectUIProviderPropsWithConnector } from "@tonconnect/ui-react";
+import {
+  TonConnectUIProvider,
+  TonConnectUIProviderPropsWithConnector,
+} from "@tonconnect/ui-react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+const manifestUrl = "https://ton.bidapool.com/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <TonConnectUIProvider>
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
